3e20abee7d3b68a8c6048874b53d9877076055c2
BradenH <bradenhonea@yahoo.com>
2021-03-09 02:42:12 -0600

    Added mouse and player movement
    While the player movement and mouse work is added, it will likely shift to rigid body (away from character controller) as if I want to implement grapple hook type tool it will probably require RB. Looking into it to decide if its needed or not

--- Movement.cs
+++ Movement.cs
@@ -4,19 +4,42 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    private Rigidbody rb;
-    // Start is called before the first frame update
-    void Start()
-    {
-        rb = GetComponent<Rigidbody>();
-    }
+    //This is the script for moving the character. Note this is meant to be in first person (FPS)
 
-    // Update is called once per frame
+    public CharacterController controller;
+    public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 5f;
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Vector3 velocity;
+    private bool isGrounded;
+
     void Update()
     {
-        if(Input.GetKey(KeyCode.W))
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if(isGrounded && velocity.y < 0)
         {
-            rb.AddForce(1, 01, 01);
+            velocity.y = -2f;
         }
+
+
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
---
+++ mousescript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class mousescript : MonoBehaviour
+{
+    //Script to control mouse movement in first person
+
+    // Start is called before the first frame update
+
+    public float sens = 100f;
+    public Transform playerModel;
+
+    private float xRotate = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sens * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sens * Time.deltaTime;
+
+        xRotate -= mouseY;
+        xRotate = Mathf.Clamp(xRotate, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotate, 0f, 0f);
+        playerModel.Rotate(Vector3.up * mouseX);
+    }
+}
